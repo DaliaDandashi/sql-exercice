7f7ccd79956a001747aee568efe9eeb4f5fb54ee
DaliaDandashi <78696190+DaliaDandashi@users.noreply.github.com>
2021-03-21 22:46:14 +0200

    Creating Table

--- answers.sh
+++ answers.sh
@@ -14,3 +14,11 @@ VALUES ("Dalia", 25, "F", 10000);
 UPDATE students set points=1000 where name="Basma";
 
 UPDATE students set points=100 where name="Alex";
+
+INSERT INTO graduates (Name,Age,Gender,Points)
+
+select name,age,gender,Points from students where name="layal";
+
+UPDATE graduates set Graduation="08/09/2018" where name="layal";
+
+DELETE from students where name="layal";
